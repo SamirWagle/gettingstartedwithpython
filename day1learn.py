ecbaed1c65fe91d744bd3d422ce9061e48c08dff
Samir Wagle <98935307+SamirWagle@users.noreply.github.com>
2022-09-09 23:29:05 +0545

    python auto identifies the data types

--- day1learn.py
+++ day1learn.py
@@ -1,2 +1,9 @@
-from playsound import playsound
-playsound('C:\\Users\Samir\Desktop\ML\Python Course with Notes\1. Chapter 1\\play.mp3')
+
+# second chapter started
+#here we are going to learn about variables and data types
+#variables are like containers that hold data
+# it auto identifies the data types
+a=5 #int
+b=6.77 #float
+c="abc"
+print(type(c)) #string
